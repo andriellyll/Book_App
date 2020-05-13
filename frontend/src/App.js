7db42f176be97f10f68ec69fe9925486c0b9f5bf
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2020-05-13 14:36:56 -0300

    Funcionalidade de pesquisar livro terminada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,45 +8,67 @@ import api from './services/api';
 
 function App () {
     const [books, setBooks] = useState([]);
-    
+    const [bookSearch, setBookSearch] = useState([]);
+    const [isSearch, setisSearch] = useState(false);
+
     async function handleRegister(data) {
+        setisSearch(false);
         await api.post('/', data);
     }
 
     async function handleDelete(id) {
+        setisSearch(false);
         await api.delete(`/delete/${id}`);
+    }
+
+    async function handleSearch(name) {
+        setisSearch(true);
+        const response = await api.get(`/search?name=${name}`);
+        setBookSearch(response.data);
+    }
+
+    function renderBooks() {
+        let booksList = []
+        
+        if(!isSearch){
+            booksList = books; 
+        } else {
+            booksList = bookSearch;
+        }
+
+        return (booksList.map(book => (
+                <Book 
+                  key={book._id}
+                  name={book.name} 
+                  author={book.author} 
+                  genre={book.genre}
+                  id={book._id}
+                  handleDelete={(id) => handleDelete(id)}
+              />
+        )));
     }
 
     // useEffect eh como o componentDidMount, componentDidupdate e componentWillUnmount (que sao usados em componentes de classes) juntos   
     // componentes de classe nao tem um metodo de executar o mesmo efeito colateral em cada renderização
+    
     useEffect(() => {
         api.get('/')
             .then(response => {
                 setBooks(response.data)
-            });
+            });    
     });
-
+    
     return (
         <div className="App">
             <div className="inputs">
                 <Form onSubmit={(data) => handleRegister(data)}/>
-                <Search/>
+                <Search onSubmit={(name) => handleSearch(name)}/>
             </div>
             <div className="books">
-                {books.map(book => (
-                  <Book 
-                    key={book._id}
-                    name={book.name} 
-                    author={book.author} 
-                    genre={book.genre}
-                    id={book._id}
-                    handleDelete={(id) => handleDelete(id)}
-                />
-                ))}
+                {renderBooks()}
             </div>
         </div>
     )
-    
 }
 
 export default App;
--- frontend/src/components/BookSearch/index.js
+++ frontend/src/components/BookSearch/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
 
-function Search() {
+function Search(props) {
+  const [name, setName] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    
+    const searchName = name;
+    setName('');
+
+    props.onSubmit(searchName);  
+  }
+
   return (
     <div className="search">
         <h3>Pesquisar livro</h3>
-        <input placeholder="Nome do Livro"/>
-        <button>Pesquisar</button>
+        <form onSubmit={e => handleSubmit(e)}>
+          <input 
+            placeholder="Nome do Livro"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <button type='submit'>Pesquisar</button>
+        </form>
     </div>
   );
 }
